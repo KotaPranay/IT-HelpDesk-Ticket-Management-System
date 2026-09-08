904d8477fedf0e8286ff1855165594d3cf849ca8
Pranay Kota <pranaykota275@gmail.com>
2026-09-08 10:21:20 +0530

    configure environment variables

--- helpdesk_system/settings.py
+++ helpdesk_system/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env" )
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+fdt9n-5@-k@p2pe17arnfj*no3r-5)e0-ugg1+3vn10_uk+6#'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
